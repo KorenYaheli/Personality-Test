00ff15ebe715a3234f058b511366853d04d73400
KorenYaheli <ramenfan03@gmail.com>
2024-11-12 15:13:39 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let contentType = 'text/html';
+    if (req.url.endsWith('.css')) contentType = 'text/css';
+    if (req.url.endsWith('.js')) contentType = 'text/javascript';
+    
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(3000, () => console.log('Server running at http://localhost:3000'));
